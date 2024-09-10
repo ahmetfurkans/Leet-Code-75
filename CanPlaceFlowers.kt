03e8a0363557fc21f7aefe8bcb06cc45fad379b8
Ahmet Furkan Sevim <78302281+ahmetfurkans@users.noreply.github.com>
2024-09-10 23:18:43 +0300

    Create CanPlaceFlowers.kt

---
+++ CanPlaceFlowers.kt
@@ -0,0 +1,39 @@
+/*
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+Example 1:
+
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+Example 2:
+
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+Constraints:
+
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+*/
+
+class Solution {
+    fun canPlaceFlowers(flowerbed: IntArray, n: Int): Boolean {
+        if(n == 0) return true
+        var slots = 0
+        for(i in 0..flowerbed.lastIndex) {
+            val prev = flowerbed.getOrNull(i-1) ?: 0
+            val next = flowerbed.getOrNull(i+1) ?: 0
+            if(prev == 0 && next == 0 && flowerbed[i] == 0) {
+                slots++
+                flowerbed[i] = 1
+            }
+
+            if(slots == n) return true
+        }
+        return false
+    }
+}
